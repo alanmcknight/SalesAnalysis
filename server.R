3ab2299c6aaa9fc7e415e8a18db10259a72baa01
Alan <mcknightalan@gmail.com>
2016-06-30 12:19:34 +0100

    Successful Upload and Download UI and Server

--- server.R
+++ server.R
@@ -1,21 +1,24 @@
-# By default, the file size limit is 5MB. It can be changed by
-# setting this option. Here we'll raise limit to 9MB.
-options(shiny.maxRequestSize = 9*1024^2)
-
-shinyServer(function(input, output) {
-  output$contents <- renderTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
+server = function(input, output) {
+  
+  data1 <- reactive({
+    if(input$Load == 0){return()}
     inFile <- input$file1
+    if (is.null(inFile)){return(NULL)}
     
-    if (is.null(inFile))
-      return(NULL)
-    
-    x <- read.csv(inFile$datapath, header = input$header,
-             sep = input$sep, quote = input$quote)
+    isolate({ 
+      input$Load
+      my_data <- read.csv(inFile$datapath, header = input$header,sep = input$sep, quote = input$quote,stringsAsFactors =FALSE)
+      #colnames(my_data) <-c("Positon.1.Provider", "Position.1.Insurer", "Position.1.Price", "Positon.2.Provider", "Position.2.Insurer", "Position.2.Price")
+      #my_data$ApricotPriceDifference <- (my_data$Position.1.Price + 1)
+      #my_data$ResponseSqRoot <- sqrt(my_data$Response + 1)
+    })
+    my_data[,c(1:5,19:21)]
   })
-})
+  output$my_output_data <- renderTable({data1()},include.rownames=FALSE)  
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { 'filtered_data.csv' }, content = function(file) {
+      write.csv(data1(), file, row.names = FALSE)
+    }
+  )
+}
--- ui.R
+++ ui.R
@@ -1,33 +1,19 @@
-shinyUI(fluidPage(
-  titlePanel("Upload File"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput('file1', 'Choose file to upload',
-                accept = c(
-                  'text/csv',
-                  'text/comma-separated-values',
-                  'text/tab-separated-values',
-                  'text/plain',
-                  '.csv',
-                  '.tsv'
-                )
-      ),
-      tags$hr(),
-      checkboxInput('header', 'Header', TRUE),
-      radioButtons('sep', 'Separator',
-                   c(Comma=',',
-                     Semicolon=';',
-                     Tab='\t'),
-                   ','),
-
-      tags$hr(),
-      p('If you want a sample .csv file to upload,',
-        'you can first download the sample',
-        a(href = 'ApricotInsuranceTemplate.csv', 'ApricotInsuranceTemplate.csv'),' and then try adding to and uploading this.'
-      )
+ui = fluidPage(
+  sidebarPanel(
+    fileInput('file1', 'Choose file to upload',accept = c('text/csv','text/comma-separated-values','text/tab-separated-values','text/plain','.csv','.tsv')),
+    p('If you want a sample .csv file to upload,',
+      'you can first download the sample',
+      a(href = 'ApricotInsuranceTemplate.csv', 'ExampleBroker.csv'),' and then try adding to and uploading this.'
     ),
-    mainPanel(
-      tableOutput('contents')
-    )
-  )
-))
+    checkboxInput('header', 'Header', TRUE),
+    radioButtons('sep', 'Separator',
+                 c(Comma=',',
+                   Semicolon=';',
+                   Tab='\t'),
+                 ','),
+    radioButtons('quote', 'Quote',c(None='','Double Quote'='"','Single Quote'="'"),'Double Quote'),
+    actionButton("Load", "Load the File"), 
+    downloadButton('downloadData', 'Download'),width = 5
+), 
+  mainPanel(tableOutput("my_output_data"))
+)
