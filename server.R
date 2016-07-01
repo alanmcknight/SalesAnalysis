89ef52875ed14d82c8a8eef7cc6b85a103d31c88
Alan <mcknightalan@gmail.com>
2016-07-01 17:25:21 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -12,13 +12,37 @@ server = function(input, output) {
       #my_data$ApricotPriceDifference <- (my_data$Position.1.Price + 1)
       #my_data$ResponseSqRoot <- sqrt(my_data$Response + 1)
     })
-    my_data[,c(1:5,19:21)]
+    #my_data
+    y <- count(my_data, c("Provider.Insurer", "Provider.Position"))
+    #y
+    ProviderInsurer <- cast(y, Provider.Insurer ~ Provider.Position)
+    ProviderInsurer
   })
+  
+  data2 <- reactive({
+    if(input$Load == 0){return()}
+    inFile <- input$file1
+    if (is.null(inFile)){return(NULL)}
+    
+    isolate({ 
+      input$Load
+      my_data1 <- read.csv(inFile$datapath, header = input$header,sep = input$sep, quote = input$quote, stringsAsFactors =FALSE)
+      #colnames(my_data) <-c("Positon.1.Provider", "Position.1.Insurer", "Position.1.Price", "Positon.2.Provider", "Position.2.Insurer", "Position.2.Price")
+      #my_data$ApricotPriceDifference <- (my_data$Position.1.Price + 1)
+      #my_data$ResponseSqRoot <- sqrt(my_data$Response + 1)
+    })
+    x <- my_data1[,c(1,19)]
+    x[,1] <- as.numeric(x[,1])
+    x[,2] <- as.numeric(x[,2])
+    x$Price.Difference <- x[,2]-x[,1]
+    x
+  })
+  
   output$my_output_data <- renderTable({data1()},include.rownames=FALSE)  
   
   output$downloadData <- downloadHandler(
-    filename = function() { 'filtered_data.csv' }, content = function(file) {
-      write.csv(data1(), file, row.names = FALSE)
+    filename = function() { 'SummaryData.csv' }, content = function(file) {
+      write.csv(data2(), file, row.names = FALSE)
     }
   )
 }
--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ ui = fluidPage(
     fileInput('file1', 'Choose file to upload',accept = c('text/csv','text/comma-separated-values','text/tab-separated-values','text/plain','.csv','.tsv')),
     p('If you want a sample .csv file to upload,',
       'you can first download the sample',
-      a(href = 'ApricotInsuranceTemplate.csv', 'ExampleBroker.csv'),' and then try adding to and uploading this.'
+      a(href = 'ApricotJan151.csv', 'ExampleBroker.csv'),' and then try adding to and uploading this.'
     ),
     checkboxInput('header', 'Header', TRUE),
     radioButtons('sep', 'Separator',
@@ -12,8 +12,8 @@ ui = fluidPage(
                    Tab='\t'),
                  ','),
     radioButtons('quote', 'Quote',c(None='','Double Quote'='"','Single Quote'="'"),'Double Quote'),
-    actionButton("Load", "Load the File"), 
-    downloadButton('downloadData', 'Download'),width = 5
+    actionButton("Load", "View Data"), 
+    downloadButton('downloadData', 'Download Summary Report'),width = 5
 ), 
   mainPanel(tableOutput("my_output_data"))
 )
