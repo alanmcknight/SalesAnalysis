7c85e33d60ef2f9694e7e31ec18aa09df3401747
Alan <mcknightalan@gmail.com>
2016-11-15 17:48:13 +0000

    Current Files Used

---
+++ Daily Processing Sales.R
@@ -0,0 +1,204 @@
+library(lubridate)
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Apricot/Daily Report Processing")
+Data <- read.csv("DailyStats.csv", stringsAsFactors =FALSE)
+
+Data$UserID <- substr(Data$BTXPolref, 1, 6)
+Data$BFDPbalance[is.na(Data$BFDPbalance)] <- 0
+Data$BTXDatecreated[Data$BTXTrantype == "Renewal"] <- as.character(Data$BTXDtraised[Data$BTXTrantype == "Renewal"])
+Data$BTXTrantype <- gsub('Transfrd NB', 'Renewal', Data$BTXTrantype)
+PolicySales <- subset(Data, (BTXTrantype == "New Business" | BTXTrantype == "Renewal") & (BTXPoltype %in% c("AG", "AR", "BD", "CA", "CC", "CL", "CP", "CR", "CV", "CW", "DO", "EB", "EL", "EN", "FL", "FP", "FS", "GT", "HH", "HL", "HO", "HQ", "HT",  "LI", "LP", "LV", "MA", "MC", "MT", "MV", "MY", "NH", "OC", "OS", "PB", "PC", "PD", "PI", "PK", "PL", "PO", "RP", "SB", "SC", "SH", "SL", "TH", "TL", "TM", "TR", "TV", "TW", "XX")))
+# PolicySales$BTXDatecreated[PolicySales$BTXTrantype == "Renewal"] <- as.character(PolicySales$BTXDtraised[PolicySales$BTXTrantype == "Renewal"])
+# PolicySales$BTXTrantype <- gsub('Transfrd NB', 'Renewal', PolicySales$BTXTrantype)
+
+x <- rbind(Data, PolicySales)
+x <- x[! duplicated(x, fromLast=TRUE) & seq(nrow(x)) <= nrow(Data), ]
+PolicySales$FinanceValue <- PolicySales$BFDPbalance * .096
+
+###### Traffic Costs and Sources
+PolicySales$TrafficCost <- 0
+PolicySales$Source <- "None"
+PolicySales$TrafficCost[grepl("APRQZ-", PolicySales$ECWebref, ignore.case=FALSE)] <- 42.50
+PolicySales$TrafficCost[grepl("APRCC-", PolicySales$ECWebref, ignore.case=FALSE)] <- 50
+PolicySales$TrafficCost[grepl("APRUS-", PolicySales$ECWebref, ignore.case=FALSE)] <- 50
+PolicySales$Source[grepl("APRQZ-", PolicySales$ECWebref, ignore.case=FALSE)] <- "Quotezone"
+PolicySales$Source[grepl("APRCC-", PolicySales$ECWebref, ignore.case=FALSE)] <- "Call Connection"
+PolicySales$Source[grepl("APRUS-", PolicySales$ECWebref, ignore.case=FALSE)] <- "Uswitch"
+PolicySales[PolicySales$BTXTrantype == "Renewal", "TrafficCost"] <- 0
+
+##### Assign Executive
+code = c(0, 1, 2, 3, 4, 5, 6, 7, 8)
+PolicySales$BTXExec[PolicySales$BTXExec == ""] <- "0"
+Executive = c("Not Assigned", "Mark", "Audrey", "Aine", "Louise", "Megan", "Elaine", "Susan", "Stephen")
+execRef <- data.frame(code, Executive)
+PolicySales <- merge(PolicySales, execRef, by.x="BTXExec", by.y="code", all.x=TRUE)
+
+PolicySales$Cancellation <- "N"
+PolicySales$CancellationCommission <- 0
+PolicySales$AddOnValue <- 0
+PolicySales$Discount <- 0
+
+PolicySales$TotalValue <- PolicySales$BTXCommamt + PolicySales$FinanceValue - PolicySales$TrafficCost
+
+#####Age Calculation
+Today <- as.Date("2016/11/01")
+PolicySales$Age = round(as.numeric((Today - as.Date(PolicySales$BCMDob, "%d/%m/%Y")) / 365.25), 0)
+
+PolicySales$Age.Range = cut(PolicySales$Age, 
+                            breaks = c(-Inf, 21, 26, 31, 36, 41, 46, 51, 56, 61, 66, 71, Inf), 
+                            labels = c("17-20", "21-25", "26-30", "31-35", "36-40", "41-45", "46-50", "51-55", "56-60", "61-65", "66-70", "70+"), 
+                            right = FALSE)
+
+#Day of Week Created Calculation
+PolicySales$Day.of.Week = weekdays(as.Date(PolicySales$BTXDatecreated, "%d/%m/%Y"))
+
+#Month Created Calculation
+#PolicySales$Month1 = month((as.Date(PolicySales$BTXDatecreated, "%d/%m/%Y")))
+PolicySales$Month1 = strftime(as.Date(PolicySales$BTXDatecreated, "%d/%m/%Y"), "%m")
+PolicySales$Month2 = months(as.Date(PolicySales$BTXDatecreated, "%d/%m/%Y"))
+PolicySales$Month <- with(PolicySales, paste0(Month1, ". ", Month2))
+PolicySales$Month1 <- NULL
+PolicySales$Month2 <- NULL
+
+#Email Domain
+PolicySales$Email.Domain = sub("^[^@]*", "", PolicySales$BCMEmail)
+
+#UK Postcode Area and Region
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Pricing Tool")
+PostcodeRegions <- read.csv("PostcodeRegions.csv")
+PolicySales$Post.Code.Prefix = gsub('[[:digit:]]+', '', substring(as.character(PolicySales$BCMPcode), 1, 2))
+PolicySales$Postcode.Area = PostcodeRegions$AREA[ match(PolicySales$Post.Code.Prefix, PostcodeRegions$POSTCODE.PREFIX)]
+PolicySales$Postcode.Region = PostcodeRegions$REGION[ match(PolicySales$Post.Code.Prefix, PostcodeRegions$POSTCODE.PREFIX)]
+
+#Quote Map Location
+Postcodes <- read.csv("ukpostcodes.csv")
+#setwd("//192.168.1.7/On-Site Home/alan.mcknight/Pricing Tool/Partner Analysis")
+PolicySales$BCMPcode <- toupper(PolicySales$BCMPcode)
+PolicySales$Longitude = Postcodes$longitude[ match(PolicySales$BCMPcode, Postcodes$postcode)]
+PolicySales$Latitude = Postcodes$latitude[ match(PolicySales$BCMPcode, Postcodes$postcode)]
+
+#write.csv(PolicySales, "DailyStatsProcessed.csv")
+
+##### COMBINE SALES REPORT
+
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Monthly Sales")
+a <- read.csv("AllSalesReport-2016-08.csv", stringsAsFactors =FALSE)
+b <- read.csv("AllSalesReport-2016-09.csv", stringsAsFactors =FALSE)
+Sales <- rbind(a, b)
+Sales$POSTCODE <- toupper(Sales$POSTCODE)
+
+DailySales <- merge(PolicySales, Sales, by.x=c("BCMDob", "BCMPcode"), by.y=c("BIRTH.DATE", "POSTCODE"), all.x=TRUE)
+
+##### COMBINE QUOTES REPORT
+
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Pricing Tool/Quote Reports")
+Quotes <- read.csv("Quotes09.csv", stringsAsFactors =FALSE)
+Quotes1 <- read.csv("Quotes10.csv", stringsAsFactors =FALSE)
+Quotes <- rbind(Quotes, Quotes1)
+Quotes$Date.Of.Birth <- gsub("[.]", "/", Quotes$Date.Of.Birth)
+DailySales <- merge(DailySales, Quotes, by.x=c("BCMDob", "BCMPcode"), by.y=c("Date.Of.Birth", "Post.Code"), all.x=TRUE)
+DailySales <- DailySales[!is.na(DailySales$BTXPolref),]
+DailySales <- within(DailySales, Source[!is.na(Quote.Reference) & Source != 'Uswitch'] <- 'Quotezone')
+DailySales <- within(DailySales, TrafficCost[!is.na(Quote.Reference) & Source != 'Uswitch' & BTXTrantype == "New Business"] <- 42.5)
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Apricot/Daily Report Processing")
+#write.csv(DailySales, "DailySales.csv", row.names=F)
+
+bspot <- which(names(DailySales)=="Price.Position.1")
+
+#UK Residency Calculation
+DailySales$Uk.Resident.Date <- gsub("[.]", "/", DailySales$Uk.Resident.Date)
+DailySales <- data.frame(DailySales[1:(bspot-1)], UK.Residency.Years = round(as.numeric((Today - as.Date(DailySales$Uk.Resident.Date, "%d/%m/%Y")) / 365.25), 0), DailySales[(bspot):ncol(DailySales)])
+
+#Licence Calculation
+DailySales$How.long.have.you.held.this.licence. <- gsub("[.]", "/", DailySales$How.long.have.you.held.this.licence.)
+DailySales <- data.frame(DailySales[1:(bspot-1)], Licence.Years = round(as.numeric((Today - as.Date(DailySales$How.long.have.you.held.this.licence., "%d/%m/%Y")) / 365.25), 0), DailySales[(bspot):ncol(DailySales)])
+
+#Vehicle Value Range Calculation
+DailySales <- data.frame(DailySales[1:(bspot-1)], Vehicle.Value.Range = cut(DailySales$What.is.the.estimated.value.of.the.vehicle., 
+                                                                            breaks = c(-Inf, 100, 500, 1000, 2000, 3000, 5000, 7000, 10000, 15000, 20000, 30000, Inf), 
+                                                                            labels = c("Less than £100", "£100-£500", "£500-£1000", "£1000-£2000", "£2000-£3000", "£3000-£5000", "£5000-£7000", "£7000-£10,000", "£10,000-£15,000", "£15,000-£20,000", "£20,000-£30,000", "£30,000 +"), 
+                                                                            right = FALSE), DailySales[(bspot):ncol(DailySales)])
+
+#DATE SOLD AND RAISED DIFFERENCE
+
+#Who.is.the.registered.keeper.of.the.vehicle.
+DailySales$Who.is.the.registered.keeper.of.the.vehicle.[grepl("^M", DailySales$Who.is.the.registered.keeper.of.the.vehicle., ignore.case=FALSE)] <- "BLANK"
+
+#Who.is.the.registered.keeper.of.the.vehicle.
+DailySales$Who.is.the.owner.of.the.vehicle.[grepl("^M", DailySales$Who.is.the.owner.of.the.vehicle., ignore.case=FALSE)] <- "BLANK"
+
+##### Apricot Position
+UploadColumnNumbers <- ncol(DailySales)
+NumberofProviders <- (ncol(DailySales)-(which( colnames(DailySales)=="Price.Position.1" )-1))/3
+for(j in 1:nrow(DailySales)){
+  ColumnMatch <- match("Apricot Agg (OGI standalone)",DailySales[j,(which( colnames(DailySales)=="Price.Position.1"):UploadColumnNumbers)])
+  if(!is.na(ColumnMatch)){
+    DailySales[j,UploadColumnNumbers+1] <- DailySales[j, ColumnMatch+93]
+    DailySales[j,UploadColumnNumbers+2] <- DailySales[j, ColumnMatch+94]
+    DailySales[j,UploadColumnNumbers+3] <- (ColumnMatch)/3
+  }
+}
+colnames(DailySales)[UploadColumnNumbers+1] <- "Selected.Provider.Price"
+colnames(DailySales)[UploadColumnNumbers+2] <- "Insurer"
+colnames(DailySales)[UploadColumnNumbers+3] <- "Apricot.Position"
+DailySales <- DailySales[, c((-1:which( colnames(DailySales)=="Price.Position.4" )+2), (ncol(DailySales)-2):(ncol(DailySales)))]
+
+#Premium Range Calculation
+DailySales <- data.frame(DailySales[1:(bspot-1)], Price.Returned.Range = cut(as.numeric(DailySales$Selected.Provider.Price), 
+                                                                             breaks = c(-Inf, 250, 500, 750, 1000, 2000, 3000, 5000, 7000, 10000, 15000, 20000, 30000, Inf), 
+                                                                             labels = c("Less than £250", "£250-£500", "£500-£750", "£750-£1000", "£1000-£2000", "£2000-£3000", "£3000-£5000", "£5000-£7000", "£7000-£10,000", "£10,000-£15,000", "£15,000-£20,000", "£20,000-£30,000", "£30,000 +"), 
+                                                                             right = FALSE), DailySales[(bspot):ncol(DailySales)])
+
+
+##### UPDATE MASTER REPORT
+setwd("//192.168.1.7/On-Site Home/alan.mcknight/Pricing Tool/SalesAnalysis")
+Master <- read.csv("ApricotSalesMasterHeaderOnly.csv", stringsAsFactors =FALSE)
+##### ADD DAILY SALES
+Master <- rbind(Master, DailySales)
+
+##### ADD ADDITIONAL PRODUCTS
+ADDITIONALPRODUCTS <- subset(x, BTXTrantype != "Cancellation" & BTXTrantype !="Endorsement" & BTXTrantype !="Journal")
+ADDON <- aggregate(as.numeric(ADDITIONALPRODUCTS$BTXCommamt), by=list(Category=ADDITIONALPRODUCTS$UserID), FUN=sum)
+Master$AddOnValue <- Master$AddOnValue + ADDON$x[match(Master$UserID, ADDON$Category)]
+Master$AddOnValue[is.na(Master$AddOnValue)] <- 0
+
+##### ADD ENDORSEMENTS AND JOURNALS
+ADDITIONALPRODUCTS2 <- subset(x, BTXTrantype == "Endorsement" | BTXTrantype == "Journal")
+ADDON2 <- aggregate(as.numeric(ADDITIONALPRODUCTS2$BTXCommamt), by=list(Category=ADDITIONALPRODUCTS2$BTXPolref), FUN=sum)
+Master$AddOnValue <- Master$AddOnValue + ADDON2$x[match(Master$BTXPolref, ADDON2$Category)]
+Master$AddOnValue[is.na(Master$AddOnValue)] <- 0
+
+##### ADD CANCELLATIONS
+CANCELLATIONS <- subset(Data, BTXTrantype == "Cancellation")
+#Master$Cancellation[match(Master$BTXPolref,CANCELLATIONS$BTXPolref)] <- "Cancellation"
+#Master$CancellationCommission[match(Master$BTXPolref,CANCELLATIONS$BTXPolref)] <- CANCELLATIONS$BTXCommamt
+
+Master <- merge(Master,CANCELLATIONS, by="BTXPolref", all.x = TRUE)
+Master <- within(Master, Cancellation[BTXTrantype.y == 'Cancellation'] <- 'Cancellation')
+Master$BTXCommamt.y[is.na(Master$BTXCommamt.y)] <- 0
+Master$BTXTrantype.y[is.na(Master$BTXTrantype.y)] <- 0
+for(i in 1:nrow(Master)){
+  if(Master$BTXTrantype.y[i] == 'Cancellation'){
+    Master$CancellationCommission[i] <- Master$BTXCommamt.y[i]
+    Master$TrafficCost[i] <- 0
+    Master$FinanceValue[i] <- 0
+    Master$AddOnValue[i] <- 0}}
+Master <- Master[,1:which( colnames(Master)=="Apricot.Position")]
+
+##### ADD DISCOUNT
+Charges <- subset(x, BTXTrantype == "Charge")
+Charges <- aggregate(as.numeric(Charges$BTXOrigdebt), by=list(Category=Charges$BTXPolref), FUN=sum)
+Master$Discount <- Master$Discount + Charges$x[match(Master$BTXPolref, Charges$Category)]
+Master$Discount[is.na(Master$Discount)] <- 0
+
+# ##### ADD DISCOUNT
+# DISCOUNT <- subset(x, BTXTrantype == "Charge")
+# Master$Discount <- Master$Discount + DISCOUNT$BTXOrigdebt[match(Master$UserID, DISCOUNT$UserID)]
+# Master$Discount[is.na(Master$Discount)] <- 0
+
+Master$TotalValue <- Master$BTXCommamt + Master$CancellationCommission + Master$FinanceValue + Master$AddOnValue - Master$TrafficCost + Master$Discount
+
+colnames(Master) <- gsub("[.]x", "", colnames(Master))
+colnames(Master) <- gsub("[.]y", "", colnames(Master))
+
+
+write.table(Master, "ApricotSalesMaster4.csv", row.names= FALSE, sep=",")
--- server.r
+++ server.r
@@ -7,7 +7,7 @@ library(plotly)
 
 server = function(input, output, session) {
   
-  my_data <- read.csv("ApricotSalesMaster3.csv", header = TRUE, stringsAsFactors =FALSE, fileEncoding="latin1")
+  my_data <- read.csv("ApricotSalesMaster4.csv", header = TRUE, stringsAsFactors =FALSE, fileEncoding="latin1")
   my_data$BTXDatecreated <- as.Date( as.character(my_data$BTXDatecreated), "%d/%m/%Y")
   
   filterList1 <- colnames(my_data) 
@@ -36,8 +36,8 @@ server = function(input, output, session) {
     colnames(Totals) <- c("New Business", "Renewals", "Renewals Outstanding", "Cancellations", "Cancellation Percentage", "Profit")
     Totals[1,1] <- nrow(subset(my_data2, Cancellation == "N" & BTXTrantype == "New Business"))
     Totals[1,2] <- nrow(subset(my_data2, Cancellation == "N" & BTXTrantype == "Renewal"))
-    Totals[1,3] <- nrow(subset(my_data1, BTXPaydt == ""))
-    Totals[1,4] <- nrow(subset(my_data2, Cancellation == "Cancellation"))
+    Totals[1,3] <- nrow(subset(my_data1, BTXPaydt == "" & BTXTrantype == "Renewal"))
+    Totals[1,4] <- nrow(subset(my_data1, Cancellation == "Cancellation"))
     Totals[1,5] <- percent(as.numeric(Totals[1,4])/(as.numeric(Totals[1,1]) + as.numeric(Totals[1,2] + as.numeric(Totals[1,4]))))
     Totals[1,6] <- currency(sum(as.numeric(my_data2$TotalValue)))
     Totals
@@ -54,44 +54,63 @@ server = function(input, output, session) {
   data8 <- reactive({
     if(length(input$dataset3) == 1){
       my_data1 <- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2] & BTXPaydt != "")
+      my_data2 <- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2])
       #if(input$filterName2 == 0){return()}
       #if(is.null(input$filterName2)) return(NULL)
       Profit <- aggregate(as.numeric(my_data1$TotalValue), by=list(Category=my_data1[[input$dataset3[1]]]), FUN=sum)
+      Profit[2] <- round(Profit[2], 2)
       Sales <- my_data1[ which(my_data1$Cancellation=='N'),]
       CountSales <- aggregate(as.numeric(Sales$TotalValue), by=list(Category=Sales[[input$dataset3[1]]]), FUN=length)
-      Cancellations <- my_data1[ which(my_data1$Cancellation=='Cancellation'),]
-      CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue), by=list(Category=Cancellations[[input$dataset3[1]]]), FUN=length)
       names(CountSales)[2]<-"Count"
-      names(CountCancellations)[2]<-"Count"
+      Cost <- aggregate(as.numeric(Sales$TrafficCost), by=list(Category=Sales[[input$dataset3[1]]]), FUN=sum)
+      names(Cost)[2]<-"TrafficCost"
       Profit$Sales <- CountSales$Count[match(Profit$Category, CountSales$Category)]
-      Profit$Cancellations <- CountCancellations$Count[match(Profit$Category, CountCancellations$Category)]
+      Cancellations <- my_data2[ which(my_data2$Cancellation=="Cancellation"),]
+      if(nrow(Cancellations) >0){
+        CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue), by=list(Category=Cancellations[[input$dataset3[1]]]), FUN=length)
+        names(CountCancellations)[2]<-"Count"
+        Profit$Cancellations <- CountCancellations$Count[match(Profit$Category, CountCancellations$Category)]
+      } else{Profit$Cancellations <- 0}
       Profit[is.na(Profit)] <- 0
       Profit[,5] <- percent(as.numeric(Profit[,4])/(as.numeric(Profit[,3])+as.numeric(Profit[,4])))
-      Profit[,6] <- Profit[,2]/(as.numeric(Profit[,3])+as.numeric(Profit[,4]))
+      Profit[,6] <- round(Profit[,2]/(as.numeric(Profit[,3])+as.numeric(Profit[,4])), 2)
+      Profit$TrafficCost <- Cost$TrafficCost[match(Profit$Category, Cost$Category)]
       #    Profit$CancellationPercentage <- as.numeric(Profit$Cancellations)/(as.numeric(Profit$Sales)+as.numeric(Profit$Cancellations))
+      Profit[7][Profit[7]==""]<- 0.00 
+      Profit$Y1Profit <- round(Profit[,2] + (Profit[,2]+Profit[,7])*0.5, 2)
+      Profit$Y2Profit <- round(Profit[,2] + (Profit[,2]+Profit[,7])*0.5 + (Profit[,2]+Profit[,7])*0.25, 2)
       names(Profit)[1]<-input$dataset3[1]
       names(Profit)[2]<-"Gross Profit (£)"
       names(Profit)[5]<-"Sales Cancellation Percentage"
       names(Profit)[6]<-"Average Gross Profit per Sale (£)"
-      Profit <- Profit[order(Profit[2]),] 
+      names(Profit)[7]<-"Traffic Cost (£)"
+      names(Profit)[8]<-"Profit inc. projected 1 year renewals (£)"
+      names(Profit)[9]<-"Profit inc. projected 2 year renewals (£)"
+      Profit <- Profit[order(Profit[1]),] 
       Profit[is.na(Profit)] <- 0
       Profit
     } else if(length(input$dataset3) == 2){
-    my_data1 <- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2] & BTXPaydt != "")
+    my_data1 <- subset(my_data, my_data$BTXDatecreated >= input$dateRange[1] & my_data$BTXDatecreated <= input$dateRange[2] & BTXPaydt != "")
+    my_data2 <- subset(my_data, my_data$BTXDatecreated >= input$dateRange[1] & my_data$BTXDatecreated <= input$dateRange[2])
     Profit <- aggregate(as.numeric(my_data1$TotalValue) ~ my_data1[[input$dataset3[1]]] + my_data1[[input$dataset3[2]]], my_data1, FUN=sum)
+    Profit[3] <- round(Profit[3], 2)
     Sales <- my_data1[ which(my_data1$Cancellation=='N'),]
     CountSales <- aggregate(as.numeric(Sales$TotalValue) ~ Sales[[input$dataset3[1]]] + Sales[[input$dataset3[2]]], Sales, FUN=length)
-    Cancellations <- my_data1[ which(my_data1$Cancellation=='Cancellation'),]
-    CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue) ~ Cancellations[[input$dataset3[1]]] + Cancellations[[input$dataset3[2]]], Cancellations, FUN=length)
-    
+    Cost<- aggregate(as.numeric(Sales$TrafficCost) ~ Sales[[input$dataset3[1]]] + Sales[[input$dataset3[2]]], Sales, FUN=sum)
     names(Profit)[1]<-input$dataset3[1]
     names(Profit)[2]<-input$dataset3[2]
     names(CountSales)[1]<-input$dataset3[1]
     names(CountSales)[2]<-input$dataset3[2]
-    names(CountCancellations)[1]<-input$dataset3[1]
-    names(CountCancellations)[2]<-input$dataset3[2]
+    names(Cost)[1]<-input$dataset3[1]
+    names(Cost)[2]<-input$dataset3[2]
     Summary <- merge(Profit, CountSales, by=c(input$dataset3[1],input$dataset3[2]), all=TRUE)
-    Summary <- merge(Summary, CountCancellations, by=c(input$dataset3[1],input$dataset3[2]), all=TRUE)
+    Cancellations <- my_data2[ which(my_data2$Cancellation=='Cancellation'),]
+    if(nrow(Cancellations) >0){
+      CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue) ~ Cancellations[[input$dataset3[1]]] + Cancellations[[input$dataset3[2]]], Cancellations, FUN=length)
+      names(CountCancellations)[1]<-input$dataset3[1]
+      names(CountCancellations)[2]<-input$dataset3[2]
+      Summary <- merge(Summary, CountCancellations, by=c(input$dataset3[1],input$dataset3[2]), all=TRUE)
+    } else{Summary$Cancellations <- 0}
     Summary[is.na(Summary)] <- 0
     Summary[,6] <- percent(as.numeric(Summary[,5])/(as.numeric(Summary[,4])+as.numeric(Summary[,5])))
     Summary[,7] <- Summary[,3]/(as.numeric(Summary[,4])+as.numeric(Summary[,5]))
@@ -100,27 +119,42 @@ server = function(input, output, session) {
     names(Summary)[5]<-"Cancellations"
     names(Summary)[6]<-"Sales Cancellation Percentage"
     names(Summary)[7]<-"Average Gross Profit per Sale (£)"
+    Summary <- merge(Summary, Cost, by=c(input$dataset3[1],input$dataset3[2]), all=TRUE)
+    Summary[8][Summary[8]==""]<- 0.00 
+    names(Summary)[8]<-"Traffic Cost (£)"
+    Summary[8][Summary[8]==""]<- 0 
+    Summary$Y1Profit <- round(Summary[,3] + (Summary[,3]+Summary[,8])*0.5, 2)
+    Summary$Y2Profit <- round(Summary[,3] + (Summary[,3]+Summary[,8])*0.5 + (Summary[,3]+Summary[,8])*0.25, 2)
+    names(Summary)[9]<-"Profit inc. projected 1 year renewals (£)"
+    names(Summary)[10]<-"Profit inc. projected 2 year renewals (£)"
     Summary <- Summary[order(Summary[3]),] 
     Summary
     } else if(length(input$dataset3) == 3){
         my_data1 <- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2] & BTXPaydt != "")
+        my_data2 <- subset(my_data, my_data$BTXDatecreated >= input$dateRange[1] & my_data$BTXDatecreated <= input$dateRange[2])
         Profit <- aggregate(as.numeric(my_data1$TotalValue) ~ my_data1[[input$dataset3[1]]] + my_data1[[input$dataset3[2]]]+ my_data1[[input$dataset3[3]]], my_data1, FUN=sum)
+        Profit[4] <- round(Profit[4], 2)
         Sales <- my_data1[ which(my_data1$Cancellation=='N'),]
         CountSales <- aggregate(as.numeric(Sales$TotalValue) ~ Sales[[input$dataset3[1]]] + Sales[[input$dataset3[2]]] + Sales[[input$dataset3[3]]], Sales, FUN=length)
-        Cancellations <- my_data1[ which(my_data1$Cancellation=='Cancellation'),]
-        CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue) ~ Cancellations[[input$dataset3[1]]] + Cancellations[[input$dataset3[2]]]+ Cancellations[[input$dataset3[3]]], Cancellations, FUN=length)
-        
+        Cost<- aggregate(as.numeric(Sales$TrafficCost) ~ Sales[[input$dataset3[1]]] + Sales[[input$dataset3[2]]] + Sales[[input$dataset3[3]]], Sales, FUN=sum)
         names(Profit)[1]<-input$dataset3[1]
         names(Profit)[2]<-input$dataset3[2]
         names(Profit)[3]<-input$dataset3[3]
         names(CountSales)[1]<-input$dataset3[1]
         names(CountSales)[2]<-input$dataset3[2]
         names(CountSales)[3]<-input$dataset3[3]
-        names(CountCancellations)[1]<-input$dataset3[1]
-        names(CountCancellations)[2]<-input$dataset3[2]
-        names(CountCancellations)[3]<-input$dataset3[3]
+        names(Cost)[1]<-input$dataset3[1]
+        names(Cost)[2]<-input$dataset3[2]
+        names(Cost)[3]<-input$dataset3[3]
         Summary <- merge(Profit, CountSales, by=c(input$dataset3[1],input$dataset3[2], input$dataset3[3]), all=TRUE)
-        Summary <- merge(Summary, CountCancellations, by=c(input$dataset3[1],input$dataset3[2], input$dataset3[3]), all=TRUE)
+        Cancellations <- my_data2[ which(my_data2$Cancellation=='Cancellation'),]
+        if(nrow(Cancellations) >0){
+          CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue) ~ Cancellations[[input$dataset3[1]]] + Cancellations[[input$dataset3[2]]]+ Cancellations[[input$dataset3[3]]], Cancellations, FUN=length)
+          names(CountCancellations)[1]<-input$dataset3[1]
+          names(CountCancellations)[2]<-input$dataset3[2]
+          names(CountCancellations)[3]<-input$dataset3[3]
+          Summary <- merge(Summary, CountCancellations, by=c(input$dataset3[1],input$dataset3[2], input$dataset3[3]), all=TRUE)
+        } else{Summary$Cancellations <- 0}
         Summary[is.na(Summary)] <- 0
         Summary[,7] <- percent(as.numeric(Summary[,6])/(as.numeric(Summary[,5])+as.numeric(Summary[,6])))
         Summary[,8] <- Summary[,4]/(as.numeric(Summary[,5])+as.numeric(Summary[,6]))
@@ -129,6 +163,14 @@ server = function(input, output, session) {
         names(Summary)[6]<-"Cancellations"
         names(Summary)[7]<-"Sales Cancellation Percentage"
         names(Summary)[8]<-"Average Gross Profit per Sale (£)"
+        Summary <- merge(Summary, Cost, by=c(input$dataset3[1],input$dataset3[2], input$dataset3[3]), all=TRUE)
+        Summary[9][Summary[9]==""]<- 0.00
+        names(Summary)[9]<-"Traffic Cost (£)"
+        Summary[9][Summary[9]==""]<- 0 
+        Summary$Y1Profit <- round(Summary[,4] + (Summary[,4]+Summary[,9])*0.5, 2)
+        Summary$Y2Profit <- round(Summary[,4] + (Summary[,4]+Summary[,9])*0.5 + (Summary[,4]+Summary[,9])*0.25, 2)
+        names(Summary)[10]<-"Profit inc. projected 1 year renewals (£)"
+        names(Summary)[11]<-"Profit inc. projected 2 year renewals (£)"
         Summary <- Summary[order(Summary[4]),] 
         Summary
       }
@@ -136,17 +178,22 @@ server = function(input, output, session) {
   
   data9 <- reactive({
     my_data9<- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2] & BTXPaydt != "")
+    my_data2 <- subset(my_data, my_data$BTXDatecreated > input$dateRange[1] & my_data$BTXDatecreated < input$dateRange[2])
       #if(input$filterName2 == 0){return()}
       #if(is.null(input$filterName2)) return(NULL)
       Profit <- aggregate(as.numeric(my_data9$TotalValue), by=list(Category=my_data9$BTXDatecreated), FUN=sum)
       Sales <- my_data9[ which(my_data9$Cancellation=='N'),]
       CountSales <- aggregate(as.numeric(Sales$TotalValue), by=list(Category=Sales$BTXDatecreated), FUN=length)
-      Cancellations <- my_data9[ which(my_data9$Cancellation=='Cancellation'),]
+      names(CountSales)[2]<-"Count"
+      Profit$Sales <- CountSales$Count[match(Profit$Category, CountSales$Category)]
+      Cancellations <- data()[ which(data()$Cancellation=='Cancellation'),]
+      if(nrow(Cancellations) >0){
       CountCancellations <- aggregate(as.numeric(Cancellations$TotalValue), by=list(Category=Cancellations$BTXDatecreated), FUN=length)
       names(CountSales)[2]<-"Count"
       names(CountCancellations)[2]<-"Count"
       Profit$Sales <- CountSales$Count[match(Profit$Category, CountSales$Category)]
       Profit$Cancellations <- CountCancellations$Count[match(Profit$Category, CountCancellations$Category)]
+      } else{Profit$Cancellations <- 0}
       Profit[is.na(Profit)] <- 0
       Profit[,5] <- percent(as.numeric(Profit[,4])/(as.numeric(Profit[,3])+as.numeric(Profit[,4])))
       Profit[,6] <- Profit[,2]/(as.numeric(Profit[,3])+as.numeric(Profit[,4]))
